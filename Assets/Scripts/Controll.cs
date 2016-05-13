dbbc3d62d47bd2ce82705c262b572c94c54a22a9
UnExisting <blackmishka2010@mail.ru>
2016-05-13 16:13:24 +0600

    бля пиздец.. как этиим пользоваться нахуй?

--- Assets/Scripts/Controll.cs
+++ Assets/Scripts/Controll.cs
@@ -2,49 +2,176 @@
 using System.Collections;
 
 public class Controll : MonoBehaviour {
+
     Rigidbody2D rig;
-    public float force= 0f;
+    public GameObject catGO;
+    public float jumpForce = 0f;
     float posX, posY;
     public int state;
-    
-    // Use this for initialization
+
+    bool facingRight = true;
+    public bool grounded = false;
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    public float dist;
+
     void Start () {
         rig = GetComponent<Rigidbody2D>();
         posX = rig.position.x;
         posY = rig.position.y;
         state = 0;
     }
-	
-	// Update is called once per frame
-	void Update () {
 
-        if (state == 0)       //можно прыгать, мы не в воздухе
+    void Update () {
+
+        /*if (state == 0)       //можно прыгать, мы не в воздухе
         {
             Debug.Log("can jump");
             if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
             {
-                rig.AddForce(new Vector3(0, force, 0));
-                state = 1;
+                rig.AddForce(new Vector3(0, jumpForce, 0));
+                state = 1;s
             }
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                rig.AddForce(new Vector3(0, force, 0));
+                rig.AddForce(new Vector3(0, jumpForce, 0));
                 state = 1;
             }
         }
         else
         {
             Debug.Log("can't jump");
+        }*/
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+      //  dist = Vector3.Distance(groundCheck.position, catGO.transform.position);
+        //col.GetComponent<BoxCollider2D>().tag
+      //  catGO.collider2D.GetComponent<BoxCollider2D>().tag;
+            if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || (Input.GetKeyDown(KeyCode.Space)))
+        {
+            Debug.Log("you've tried(");
+            
+            if (grounded)
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+            }
         }
-        
+
+        if (Input.GetKey(KeyCode.Escape))
+        {   
+            Application.Quit();
+        }
+
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
+
         if (col.GetComponent<BoxCollider2D>().tag == "Wall") {
             rig.position = new Vector2(posX,0);
         }
     }
+
+  /*  void OnTriggerEnter2D(Collider2D col)
+    {
+        if ((col.gameObject.name == "dieCollider") || (col.gameObject.name == "saw"))
+            Application.LoadLevel(Application.loadedLevel);
+
+
+        if (col.gameObject.name == "endLevel")
+        {
+            if (!(GameObject.Find("star"))) Application.LoadLevel("scene2");
+        }
+    }*/
+
 }
 
+
+   /* using UnityEngine;
+using System.Collections;
+
+public class characterController : MonoBehaviour {
+	public float maxSpeed = 10f;
+	public float jumpForce = 700f;
+	bool facingRight = true;
+	bool grounded = false;
+	public Transform groundCheck;
+	public float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	public float score;
+	public float move;
+
+	private GameObject star;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+
+		move = Input.GetAxis ("Horizontal");
+
+	}
+
+	void Update(){
+		if (grounded && (Input.GetKeyDown (KeyCode.W)||Input.GetKeyDown (KeyCode.UpArrow))) {
+
+			GetComponent<Rigidbody2D>().AddForce (new Vector2(0f,jumpForce));
+		}
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		
+		if (move > 0 && !facingRight)
+			Flip ();
+		else if (move < 0 && facingRight)
+			Flip ();
+
+
+
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+
+		if (Input.GetKey(KeyCode.R))
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}
+
+
+	}
+	
+	void Flip(){
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if ((col.gameObject.name == "dieCollider")||(col.gameObject.name == "saw"))
+						Application.LoadLevel (Application.loadedLevel);
+
+	    if (col.gameObject.name == "star") {
+						score++;
+						Destroy (col.gameObject);
+				}
+
+		if (col.gameObject.name == "endLevel") {
+			if (!(GameObject.Find("star"))) Application.LoadLevel ("scene2");
+				}
+	}
+
+	void OnGUI(){
+				GUI.Box (new Rect (0, 0, 100, 100), "Stars: " + score);
+		}
+		
+}
+
+
+    */
--- Assets/Scripts/CreateBox.cs
+++ Assets/Scripts/CreateBox.cs
@@ -4,63 +4,106 @@ using System;
 
 public class CreateBox : MonoBehaviour
 {
-    public GameObject boxExample;
-    public float range;
+    public GameObject boxExample;   //перфаб коробки
+    public Sprite boomExample;  //перфаб(спрайт) взрыва
 
-    GameObject[] targets = new GameObject[5];
+    public float range; //радиус окружности. нужен для правильного появления коробок
+
+    public float speedRotationBox;  //скорость вращения бочки в верхней-левой части экрана
+
+    public int countAllBoxes;       //кол-во бочек
+    GameObject[] targets;       //объекты коробки
+    int[] boxState;     //состояние коробки (0=нету, 1= есть одна, 2=есть две)
+    bool[] boxChecked;  //прошёл ли кубик проверку(удалить его или нет). нужно для оптимизации и вращения
     int myTimer;
-    int minNumOfBoxes;
-    int activeBoxes;
+
+    Sprite boomSpr;
+    public bool isBoom;
+
 
     void Start()
     {
-        range = 1.4f;
-        // AddBox();
-        activeBoxes = 0;
+        isBoom = false;
+        targets = new GameObject[countAllBoxes];
+        boxChecked = new bool[countAllBoxes];
+        boxState = new int[countAllBoxes];
+        for (int i = 0; i < countAllBoxes; i++) 
+        {
+            boxState[i] = 0;
+            boxChecked[i] = true;
+        }
+        
+        range = 1.32f;   
         myTimer = 0;
-        minNumOfBoxes = 0;
     }
 
     void Update()
     {
         myTimer++;
-        if (myTimer >= 150)     //usualy we will create a boxes
-        {
+        if (myTimer >= 120)     //usualy we will create a boxes
+        {                           //max count == countAllBoxes
             myTimer = 0;
-            AddBox(minNumOfBoxes);
-
-            minNumOfBoxes++;
-            if (minNumOfBoxes >= 5)
-                minNumOfBoxes -= minNumOfBoxes;
+            for (int i = 0; i< countAllBoxes; i++)            
+                if (boxState[i] == 0)
+                {
+                    AddBox(i);
+                    break;
+                }      
         }
-       /* foreach (var item in collection)
-        {
 
-        }*/
-        for (int i = 0; i < activeBoxes; i++)       //chek EACH EXISTED box for ending
+        for (int i = 0; i < countAllBoxes; i++)
         {
-            if ((targets[i] != null) && (targets[i].transform.position.x > 1f))       //check to delete
+            if (boxState[i] == 1)   //если коробка существует
             {
-                Debug.Log("i'm out!");
-                DeleteBox(i);
+                if (boxChecked[i] == false) //и она не прошла проверку(она находится слева или внизу)
+                {
+                    if (targets[i].transform.position.x > 1.2f) //и она дошла до точки справа
+                    {
+                        if (UnityEngine.Random.Range(0, 2) == 0)    //удалить или нет?
+                        {
+                            boxState[i] = 0;    
+                            DeleteBox(i);       //удаляем
+                            Debug.Log("i'm out!");
+                        }
+                        else
+                        {
+                            boxChecked[i] = true;   //хочу вращаться 
+                        }
+                    }
+                }
+                else
+                {
+                    targets[i].transform.Rotate(new Vector3(0, 0, speedRotationBox) * Time.deltaTime);  //йююююху
+                    if (targets[i].transform.position.x < -1.2f)    //если она дошла до рабочей  части экрана(рядом с кошой), то стоит перестать это делать
+                    {
+                        boxChecked[i] = false;
+                    }
+                }
             }
         }
+
+      // check =  UnityEngine.Random.Range(0, 2);
     }
 
-    void AddBox(int n)
+    void AddBox(int n)  
     {
+        boxState[n] = 1;//для начального вращения
         Debug.Log("new box");
-        targets[n] = new GameObject();
-        targets[n] = (GameObject) Instantiate(boxExample, new Vector2(0, range), Quaternion.identity);
-        targets[n].transform.parent = GameObject.Find("Boxes").transform ;
-        activeBoxes++;
+        targets[n] = (GameObject) Instantiate(boxExample, new Vector2(0, range), Quaternion.identity);//создаёт кубик
+        targets[n].transform.parent = GameObject.Find("Boxes").transform ;  //привязывыаем его к платформе)
+
+
     }
 
-    void DeleteBox(int n)
+    void DeleteBox(int n)//для добавления взрыва после смерти кубика
     {
-        Destroy(targets[n]);
-        activeBoxes--;
-      //  Destroy(gameObject);
+    //    boomSpr = (Sprite)Instantiate(boomExample, targets[n].transform.position, targets[n].transform.rotation);
+        Destroy(targets[n]);        
+    }
+
+    void DeleteBoom()
+    {
+        Destroy(boomSpr);
     }
 
 }
